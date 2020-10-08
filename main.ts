67b10986267a76f7354e91aedae39d9942975af3
Mikayla-Barthelette <71400623+Mikayla-Barthelette@users.noreply.github.com>
2020-10-08 14:19:06 -0400

    final commit

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 let potentiometer = robotbit.RgbUltrasonic(DigitalPin.P1)
 robotbit.Servo(robotbit.Servos.S1, 0)
 basic.showIcon(IconNames.Yes)
+basic.pause(1000)
 basic.forever(function () {
-    let servo = 0
-    basic.showNumber(servo)
+    basic.showNumber(potentiometer)
     robotbit.Servo(robotbit.Servos.S1, potentiometer)
+    basic.pause(500)
 })
