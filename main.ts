ec56357e50350d0410e2deebb31b616103dfffca
Mikayla-Barthelette <71400623+Mikayla-Barthelette@users.noreply.github.com>
2020-10-08 10:10:34 -0400

    added potentiometer and servo

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let potentiometer = robotbit.RgbUltrasonic(DigitalPin.P1)
+robotbit.Servo(robotbit.Servos.S1, 0)
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    let servo = 0
+    basic.showNumber(servo)
+    robotbit.Servo(robotbit.Servos.S1, potentiometer)
 })
