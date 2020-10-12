e8aaac89ac7af2d657928b3dcdefd6d8b045a209
Mikayla-Barthelette <71400623+Mikayla-Barthelette@users.noreply.github.com>
2020-10-12 15:01:20 -0400

    final commit

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
-let potentiometer = robotbit.RgbUltrasonic(DigitalPin.P1)
-robotbit.Servo(robotbit.Servos.S1, 0)
+// Created by: Mikayla Barthelette
+// Created on: Oct 2020
+// 
+// This program allows us to use a potentiometer to control a servo
+let angle = 0
+let potentiometer = 0
 basic.showIcon(IconNames.Yes)
-basic.pause(1000)
+basic.pause(500)
 basic.forever(function () {
-    basic.showNumber(potentiometer)
-    robotbit.Servo(robotbit.Servos.S1, potentiometer)
-    basic.pause(500)
+    robotbit.Servo(robotbit.Servos.S1, 0)
+    potentiometer = pins.analogReadPin(AnalogPin.P1)
+    angle = potentiometer * (360 / 1023)
+    robotbit.Servo(robotbit.Servos.S1, angle)
+    basic.showNumber(angle)
+    basic.pause(3000)
 })
